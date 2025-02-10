12ef18f86622b634b7982ec51d5eb812f63d0dd8
harrismlyd <harris_ita@yahoo.com.sg>
2025-02-10 21:20:25 +0800

    Modify main.tf file

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ provider "aws" {
 }
 
 terraform {
+ required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+
   backend "s3" {
     bucket = "sctp-ce8-tfstate"
     key    = "harris-s3-tf-ci.tfstate" #Change this
@@ -13,10 +21,10 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  name_prefix = split("/", "data.aws_caller_identity.current.arn")[1]
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+  bucket = local.name_prefix+"-s3-tf-bkt-"+local.account_id
 }
